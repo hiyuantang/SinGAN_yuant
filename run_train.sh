1673301989eb1e88adf8bab6e1c3e2cbc42fcded
Yuan Tang <yuantang@g.harvard.edu>
2023-12-14 00:04:26 -0500

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,20 @@
-python src/train.py --input_image balloons.png
+# Make sure cd into the src directory to run this bash shell
+
+# Models trained on balloons
+python train.py --input_image balloons_downsampled.png # model 1
+python train_patches.py --input_image balloons_downsampled.png # model 2
+
+python train.py --input_image balloons_downsampled.png --number_of_noises 12 # model 3
+python train.py --input_image balloons_downsampled.png --scale_factor 0.35 # model 4
+python train.py --input_image balloons_downsampled.png --alpha 1 # model 5
+python train.py --input_image balloons_downsampled.png --number_of_noises 3 # model 6
+
+
+
+# Models trained on pyramids
+python train.py --input_image pyramids_downsampled.jpg # model 1
+python train_patches.py --input_image pyramids_downsampled.jpg # model 2
+
+python train.py --input_image pyramids_downsampled.jpg --number_of_noises 12 # model 3
+python train.py --input_image pyramids_downsampled.jpg --scale_factor 0.35 # model 4
+python train.py --input_image pyramids_downsampled.jpg --alpha 1 # model 5
