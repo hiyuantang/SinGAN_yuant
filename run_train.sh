0f1cf42e662b35048e970e85d617ac5367803c60
Yuan Tang <yuantang@g.harvard.edu>
2023-12-14 00:14:35 -0500

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -4,10 +4,9 @@
 python train.py --input_image balloons_downsampled.png # model 1
 python train_patches.py --input_image balloons_downsampled.png # model 2
 
-python train.py --input_image balloons_downsampled.png --number_of_noises 12 # model 3
+python train.py --input_image balloons_downsampled.png --number_of_noises 3 # model 3
 python train.py --input_image balloons_downsampled.png --scale_factor 0.35 # model 4
 python train.py --input_image balloons_downsampled.png --alpha 1 # model 5
-python train.py --input_image balloons_downsampled.png --number_of_noises 3 # model 6
 
 
 
@@ -15,6 +14,6 @@ python train.py --input_image balloons_downsampled.png --number_of_noises 3 # mo
 python train.py --input_image pyramids_downsampled.jpg # model 1
 python train_patches.py --input_image pyramids_downsampled.jpg # model 2
 
-python train.py --input_image pyramids_downsampled.jpg --number_of_noises 12 # model 3
+python train.py --input_image pyramids_downsampled.jpg --number_of_noises 3 # model 3
 python train.py --input_image pyramids_downsampled.jpg --scale_factor 0.35 # model 4
 python train.py --input_image pyramids_downsampled.jpg --alpha 1 # model 5
